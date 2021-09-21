dbf0ca6ca48ec64aa89773b139bf023778380715
albertoparente <albertoparentefh@gmail.com>
2021-09-20 23:54:52 -0300

    Removing Test

---
+++ src/pages/Stylish.jsx
@@ -0,0 +1,9 @@
+import stylish from './Stylish.module.css' 
+
+export default function Stylish() {
+    return (
+        <div>
+            <h1>Style using CSS modules</h1>
+        </div>
+    )
+}
